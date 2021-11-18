c05b820c58cd1cc9e6c4ee889ed47a53494c70f8
beautyesther <ucheesther87@gmail.com>
2021-11-18 23:05:03 +0100

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  tableData: any | undefined;
+   pageIndex = 0;
   title = 'TransMonitor';
 }
